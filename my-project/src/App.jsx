a01e18b1aa925205aad0ad452880f8e850775060
Fatima <fatimanaeemulhaq10@gmail.com>
2025-09-15 23:52:54 -0700

    Projects Section added

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import {gsap} from 'gsap' ;
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import CustomCursor from "./components/customCursor";
 import AboutSection from "./components/AboutSection";
 import Project from "./components/Project";
 export default function App() {
+
+
+   useEffect(()=>{
+       gsap.registerPlugin(ScrollTrigger)
+       ScrollTrigger.refresh()
+       return () =>{
+        ScrollTrigger.getAll().forEach((trigger)=> trigger.kill())
+       }
+    } ,[])
+
   return (
      <>
      <Header />
