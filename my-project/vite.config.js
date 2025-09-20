8b72ff8ced211e06a636acc8b7c8e39528de0138
Fatima <fatimanaeemulhaq10@gmail.com>
2025-09-20 11:50:43 -0700

    Update vite.config.js and package.json for deployment

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Fatima-Code-Chaos/',   // 👈 Important for GitHub Pages
+  base: '/Portfoilio-Fatima_Code-Chaos/', // 👈 repo name goes here
 })
